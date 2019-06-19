cdedcb5ae6fc17c12ff00128e5c47409f9567b01
Harjot Singh Na <747998@student.tor.loc>
2019-06-19 14:08:18 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello Class');
+//basic way to create an object
+const circle = {
+    redius :1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function(){
+        console.log('draw')
+    }
+}; 
