faddbee7574e35e17b76fbe5d71d753f99f485fc
Harjot Singh Na <747998@student.tor.loc>
2019-06-19 16:16:10 -0400

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-//basic way to create an object
-const circle = {
-    redius :1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function(){
-        console.log('draw')
-    }
-}; 
+// factory function
+function createCircle(radius){
+    return {
+       radius,
+       draw: function(){
+           console.log('draw');
+       }
+   };   
+   }
+   const circle = createCircle(1);
+   circle.draw();
+   
+   // Constructor Function
+   
+   function Circle(radius){
+       console.log('this', this);
+       this.radius = radius;
+       this.draw = function(){
+           console.log("draw");
+       }
+   }
+   
+   const Circle1 = new Function('radius',`
+   this.radius = radius;
+   this.draw = function(){
+       console.log("draw");
+   }
+   `)
+   const circle1 = new Circle1(1);
+   const another = new Circle(1);
+   
